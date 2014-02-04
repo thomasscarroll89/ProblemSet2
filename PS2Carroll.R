7c207d0b433563b2ca84c4cc945edd9e2affbcd4
thomasscarroll89 <thomasscarroll89>
2014-02-04 12:54:41 -0600

    Problem 2 Changes
    Mostly changes to problem 2. Included comments for the problem. Also
    changed some of the cat() functions to paste() because the cat()
    function, for some reason, was not able to be plugged into the objects
    m.significances and d.significances. Also modified problem 1 to include
    the square root of n in the equations (as on facebook page). NOTE TO
    SELF: Still need a way for the table output from problem 2 to line up
    better when the number of stars are different.

--- PS2Carroll.R
+++ PS2Carroll.R
@@ -15,6 +15,7 @@ BenfordsLaw <- function(data.matrix, M=TRUE, D=TRUE){
   #into the elements of the output list created above
   for(i in 1:ncol(data.matrix)){
     charactervector <- data.matrix[,i] #selects i'th column
+    n <- length(charactervector) #calculates n as sample size, necessary to use the modified formula posted on facebook
     FirstSignificantDigit <- substr(charactervector, 1, 1) #selects only 1st significant digit in each observation
     proportions <- numeric(9) 
     numbers <- numeric(9)
@@ -22,8 +23,8 @@ BenfordsLaw <- function(data.matrix, M=TRUE, D=TRUE){
       proportions[k] <- sum(FirstSignificantDigit==k)/length(FirstSignificantDigit) #calculate the X_i from Benford's equation
       numbers[k] <- proportions[k] - log(1 + 1/k, base=10) #calculate the vector of numbers used to calculate the 2 statistics
     }  
-    m.statistic <- max(numbers) #calculate Leemis' statistic
-    d.statistic <- sqrt(sum((numbers)^2)) #calculate Cho-Gains statistic
+    m.statistic <- sqrt(n)*max(numbers) #calculate Leemis' statistic
+    d.statistic <- sqrt(n)*sqrt(sum((numbers)^2)) #calculate Cho-Gains statistic
     
     #Next set of for loops gives different instructions on what to store in the output depending on whether M or D (or both)
     #was set to TRUE
@@ -44,59 +45,70 @@ BenfordsLaw <- function(data.matrix, M=TRUE, D=TRUE){
 }
 
 ####Problem 2####
-
 print.benfords <- function(data.matrix, M=TRUE, D=TRUE){
-  stored.output <- BenfordsLaw(data=data.matrix, M=M, D=D)
+  stored.output <- BenfordsLaw(data=data.matrix, M=M, D=D) #First run the function from problem 1. 
+                  #Store the output from this function in a list called stored.output. Each element
+                  #of the list corresponds to the output for one of the columns of the matrix input. 
   if(M == TRUE){
-    m.statistics <- numeric(length(stored.output))
-    m.significances <- numeric(length(stored.output))
+    m.statistics <- numeric(length(stored.output))    #Create 2 empty numeric vector whose lengths are 
+    m.significances <- numeric(length(stored.output)) #equal to the number of columns of the matrix input
+                                                      
     for(i in 1:length(stored.output)){
-      m.statistics[i] <- sprintf("%.3f", round(stored.output[[i]]$Leemis, 5))
+      m.statistics[i] <- sprintf("%.3f", round(stored.output[[i]]$Leemis, 5)) 
+              #plug into m.statistics the Leemis' values. The sprintf() function keeps trailing 0s
+      #Next few if() statements basically add asterisks (*) to denote the significance of the Leemis'
+        #statistics based on their size
+    }
+    for(i in 1:length(stored.output)){
       if(m.statistics[i] < 0.851){
         m.significances[i] <- m.statistics[i]
       }
       if(m.statistics[i] >= 0.851 & m.statistics[i] < 0.967){
-        m.significances[i] <- cat(m.statistics[i], "*", sep="")
+        m.significances[i] <- paste(m.statistics[i], "*", sep="")
       }
       if(m.statistics[i] >= 0.967 & m.statistics[i] < 1.212){
-        m.significances[i] <- cat(m.statistics[i], "**", sep="")
+        m.significances[i] <- paste(m.statistics[i], "**", sep="")
       }
       if(m.statistics[i] >= 1.212){
-        m.significances[i] <- cat(m.statistics[i], "***", sep="")
+        m.significances[i] <- paste(m.statistics[i], "***", sep="")
       } 
     }  
   }
   
+  #This next if statement does the same as the last, only for the ChoGains' statistics. 
   if(D == TRUE){
     d.statistics <- numeric(length(stored.output))
     d.significances <- numeric(length(stored.output))
     for(i in 1:length(stored.output)){
       d.statistics[i] <- sprintf("%.3f", round(stored.output[[i]]$ChoGains, 5))
+    }
+    for(i in 1:length(stored.output)){
       if(d.statistics[i] < 1.212){
         d.significances[i] <- d.statistics[i]
       }
       if(d.statistics[i] >= 1.212 & d.statistics[i] < 1.330){
-        d.significances[i] <- cat(d.statistics[i], "*", sep="")
+        d.significances[i] <- paste(d.statistics[i], "*", sep="")
       }
       if(d.statistics[i] >= 1.330 & d.statistics[i] < 1.569){
-        d.significances[i] <- cat(d.statistics[i], "**", sep="")
+        d.significances[i] <- paste(d.statistics[i], "**", sep="")
       }
       if(d.statistics[i] >= 1.569){
-        d.significances[i] <- cat(d.statistics[i], "***", sep="")
+        d.significances[i] <- paste(d.statistics[i], "***", sep="")
       } 
     }  
   }  
   
+  #Finally print the results in a table that gets output into the R console. 
   cat(cat("\nLeemis' M      ", m.significances, 
       "\nCho-Gains' D   ", d.significances, sep="       "), 
       "\n\nSignif. Codes: '***' 0.001, '**' 0.05, '*' 0.1, '' 1")
 }
 
-test1 <- rnorm(50000, 50000, 25000)
-test2 <- rnorm(50000, 50000, 25000)
-test3 <- rnorm(50000, 50000, 25000)
-test4 <- rnorm(50000, 50000, 25000)
-test5 <- rnorm(50000, 50000, 25000)
+test1 <- rnorm(100, 50000, 25000)
+test2 <- rnorm(100, 50000, 25000)
+test3 <- rnorm(100, 50000, 25000)
+test4 <- rnorm(100, 50000, 25000)
+test5 <- rnorm(100, 50000, 25000)
 Test <- cbind(test1, test2, test3, test4, test5)
 print.benfords(Test)
 
